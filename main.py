bb9be550e7e7c0e5504b0868a5f0e240bb98d6dd
Zehida120 <95170812+Zehida120@users.noreply.github.com>
2022-05-27 17:35:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Data visualization with matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-# Scatter plot using axes method
+# Scatter plot using axes object 
 pd_file=pd.read_csv('abs.csv')
 pd_file.index=pd_file['country']
 pd_file['year']=pd.to_datetime(pd_file['year'], infer_datetime_format=True)
@@ -13,12 +13,12 @@ ax.tick_params('x', color='black')
 ax.set_yticklabels(pd_file['living space'], color='black')
 ax.tick_params('y', color='black')
 fig.savefig('scatter.png')
-# Bar plot using axes method
+# Bar plot using axes object 
 fig, ax=plt.subplots()
 ax.bar(pd_file.index, pd_file['zip'], color='black')
 ax.set_xticklabels(pd_file.index, rotation=90)
 plt.show()
-# subplots using axes method
+# subplots using axes object 
 fig, ax=plt.subplots(2, 1)
 ax[0].plot(pd_file.index, pd_file['living space'], color='black', linestyle='--', marker='o')
 ax[0].set_xticklabels(pd_file.index, rotation=90)
@@ -32,12 +32,12 @@ ax.set_xticklabels(pd_file.index, rotation=90)
 ax2=ax.twinx()
 ax2.plot(pd_file.index, pd_file['baths'], color='black', linestyle='--')
 plt.show()
-# boxplot using axes method
+# boxplot using axes object
 fig, ax=plt.subplots()
 ax.boxplot([pd_file['living space'], pd_file['zip']])
 ax.set_xlabel(['living space', 'zip'])
 fig.savefig('box.png')
-# Error bar using axes method
+# Error bar using axes object 
 fig, ax=plt.subplots()
 ax.errorbar(pd_file.index, pd_file['beds'], yerr=pd_file['zip'])
 ax.set_xticklabels(pd_file.index, rotation=90)
@@ -45,7 +45,7 @@ fig.savefig('errorbar.png')
 fig, ax=plt.subplots()
 ax.bar(pd_file['medals'], pd_file['beds'], yerr=pd_file['baths'])
 fig.savefig('bar.png')
-# Histogram using axes method
+# Histogram using axes object 
 fig, ax=plt.subplots()
 ax.hist(pd_file['zip'], histtype='step', label='ZIP')
 ax.set_xlabel('zip')
